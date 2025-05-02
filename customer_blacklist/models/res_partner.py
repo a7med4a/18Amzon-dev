98f45be8f5fb07b3678d2b29f2735e35a682319c
AliEl-Mohammady <ali01025893594@gmail.com>
2025-05-02 20:59:35 +0300

    [update] customer_blacklist

--- customer_blacklist/models/res_partner.py
+++ customer_blacklist/models/res_partner.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import fields, models, api
+from lxml import etree
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
@@ -9,3 +10,17 @@ class ResPartner(models.Model):
         ("blocked", "Blocked")], required=False, default="running", readonly=True,store=True)
     blacklist_reason = fields.Text(string='Blacklist Reason', readonly=True)
     blacklist_history_ids = fields.One2many('res.partner.blacklist.history', 'partner_id', string='Blacklist History')
+
+    @api.model
+    def get_view(self, view_id=None, view_type='form', **options):
+        res = super().get_view(view_id, view_type, **options)
+        if (view_type == 'form' or view_type == 'list') and options.get('action_id') == self.env.ref('customer_blacklist.rental_individual_customers_action_blocked').id:
+            doc = etree.XML(res['arch'])
+            list_view = doc.xpath("//list")
+            form_view = doc.xpath("//form")
+            if list_view:
+                list_view[0].set("create", "0")
+            if form_view:
+                form_view[0].set("create", "0")
+            res['arch'] = etree.tostring(doc, encoding='unicode')
+        return res
